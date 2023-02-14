737ca0710800a0ca96e6304ee04e96acd98b39b2
USERSAD\apallarl <andres_pallares@hotmail.com>
2023-02-14 08:49:14 +0100

    Compilando typescript

---
+++ app.js
@@ -0,0 +1,5 @@
+console.log("**log de pruebas compilando typescript**");
+var mensaje = "prueba javascript";
+var layout = document.querySelector('#entrada'); //operador de asercion nula, esta expresion no va a ser nula
+console.log("layout");
+console.log(layout.children);
---
+++ app.ts
@@ -0,0 +1,17 @@
+
+
+
+
+console.log("**log de pruebas compilando typescript**")    
+
+const mensaje="prueba javascript"
+
+
+const layout = document.querySelector('#entrada')!   //operador de asercion nula, esta expresion no va a ser nula
+
+console.log("layout")
+console.log(layout.children)
+
+
+
+
