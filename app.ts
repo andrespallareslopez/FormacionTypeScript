03af676e8a9ee7b1b4c8c0693002c7b6a2a64619
USERSAD\apallarl <andres_pallares@hotmail.com>
2023-02-14 08:57:06 +0100

    Actualizando pruebas

--- app.ts
+++ app.ts
@@ -4,8 +4,20 @@
 
 console.log("**log de pruebas compilando typescript**")    
 
-const mensaje="prueba javascript"
+let mensaje="prueba typescript con tipado debil o inferencia implicita"   //tipo cadena que lo esta infiriendo implicitamente
 
+let mensaje1: string ="prueba typescript con tipado fuerte"
+
+//vamos a poner que pueda ser un numero y un string 
+let valor: string | number = 12000
+
+
+
+mensaje="hola"    //si intentamos poner un numero le ponemos numero nos va a dar error
+
+
+console.log(mensaje)
+console.log(mensaje1)
 
 const layout = document.querySelector('#entrada')!   //operador de asercion nula, esta expresion no va a ser nula
 
