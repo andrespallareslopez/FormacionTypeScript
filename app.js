01c5c9bc1d44d2089f257c77c348336edb1e8799
USERSAD\apallarl <andres_pallares@hotmail.com>
2023-02-14 16:52:15 +0100

    Actualizar apuntes type script

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-console.log("**log de pruebas compilando typescript**");
-var mensaje = "prueba typescript con tipado debil o inferencia implicita"; //tipo cadena que lo esta infiriendo implicitamente
-mensaje = "hola"; //si intentamos poner un numero le ponemos numero nos va a dar error
+//tsc app.ts -w
+console.log("**log de pruebas typecasting con typescript**");
+var mensaje = "prueba javascript";
 console.log(mensaje);
