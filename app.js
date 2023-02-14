a0fb092dc5efa72dfc19fb630f2dacc40e2ebdbc
USERSAD\apallarl <andres_pallares@hotmail.com>
2023-02-14 09:05:31 +0100

    apuntes tipos basicos

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 console.log("**log de pruebas compilando typescript**");
 var mensaje = "prueba typescript con tipado debil o inferencia implicita"; //tipo cadena que lo esta infiriendo implicitamente
+var mensaje1 = "pruebas typescript con tipado fuerte";
+var cantidad = 1000;
+var numeros = [1, 2, 3, 4, 5, 6];
 mensaje = "hola"; //si intentamos poner un numero le ponemos numero nos va a dar error
 console.log(mensaje);
+console.log(cantidad);
+console.log(numeros);
+numeros.forEach(function (e) {
+    console.log(e);
+});
--- app.ts
+++ app.ts
@@ -6,13 +6,22 @@ console.log("**log de pruebas compilando typescript**")
 
 let mensaje="prueba typescript con tipado debil o inferencia implicita"   //tipo cadena que lo esta infiriendo implicitamente
 
+let mensaje1: string = "pruebas typescript con tipado fuerte"
+
+let cantidad: number = 1000
+
+let numeros: number[] = [1,2,3,4,5,6]
 
 mensaje="hola"    //si intentamos poner un numero le ponemos numero nos va a dar error
 
 
 console.log(mensaje)
+console.log(cantidad)
+console.log(numeros)
 
-
+numeros.forEach((e) => {
+    console.log(e)
+})
 
 
 
