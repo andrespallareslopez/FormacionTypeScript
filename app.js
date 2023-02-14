02fa9e9db7b11f91467b28d08af93c3f6ed121c1
USERSAD\apallarl <andres_pallares@hotmail.com>
2023-02-14 16:57:31 +0100

    Actualizar apuntes de typescript

--- app.js
+++ app.js
@@ -2,3 +2,9 @@
 console.log("**log de pruebas typecasting con typescript**");
 var mensaje = "prueba javascript";
 console.log(mensaje);
+//sabe que es un HTMLPreElement porque se lo estamos diciendo en
+//query selector y lo infiere implicitamente y le pongo el operador de asercion
+//nula, porque no sabe si de inicio va a decolver algo
+var output = document.querySelector("pre");
+output.innerHTML = mensaje;
+//if (output) output.innerHTML=mensaje
--- app.ts
+++ app.ts
@@ -8,3 +8,12 @@ const mensaje="prueba javascript"
 
 console.log(mensaje)
 
+//sabe que es un HTMLPreElement porque se lo estamos diciendo en
+//query selector y lo infiere implicitamente y le pongo el operador de asercion
+//nula, porque no sabe si de inicio va a decolver algo
+const output= document.querySelector("pre")!
+
+output.innerHTML=mensaje
+
+//if (output) output.innerHTML=mensaje
+
